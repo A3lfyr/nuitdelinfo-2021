1631ff19e07c9d3e2e8123217b0eb6a4d0b36828
Vadot Dylan <vadotdyl@cvut.cz>
2021-12-03 01:08:59 +0100

    Ajout components

---
+++ nuit-info/src/components/PageSauveteur.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+  name: 'PageSauveteur',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
