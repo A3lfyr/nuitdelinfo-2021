be2c6c5419175cc4706f8ce74a925cdcecaf735b
mathlam <mathilde_lamirault@icloud.com>
2021-12-03 03:09:10 +0100

    sauvetage endpoints

--- backend/node-express-mongoose-starter2/src/routers/sauvetage.router.js
+++ backend/node-express-mongoose-starter2/src/routers/sauvetage.router.js
@@ -3,15 +3,38 @@ const router = new express.Router()
 const Sauvetage = require('../models/Sauvetage.model')
 
 // Create user / Signup
-router.get('/sauvetage', async (req, res) => {
+router.get('/sauvetage/:id', (req, res) => {
+    Sauvetage.findOne({ _id: req.params.id })
+        .then(sauvetage => res.status(200).json(sauvetage))
+        .catch(error => res.status(404).json({ error }));
+});
 
-    try {
-        await new Sauvetage({ random: Math.random() }).save()
-        const randoms = await Sauvetage.find({})
-        return res.status(201).send(randoms.map(random => random.random))
-    } catch (error) {
-        res.status(400).send({error: 'Oh no! An error!', error: error.message})
-    }
-})
+router.get('/sauvetages', (req, res, next) => {
+    Sauvetage.find()
+      .then(sauvetages => res.status(200).json(sauvetages))
+      .catch(error => res.status(400).json({ error }));
+});
+
+router.post('/sauvetage', (req, res) => {
+    delete req.body._id;
+    const sauvetage = new Sauvetage({
+      ...req.body
+    });
+    sauvetage.save()
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
+});
+
+router.put('/sauvetage/:id', (req, res, next) => {
+    Sauvetage.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+});
+
+router.delete('/sauvetage/:id', (req, res, next) => {
+    Sauvetage.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+});
 
 module.exports = router
